5c8f348c3463d7815485d19cfcd1bb33ce2bde49
JohannesDoberer <johannes.doberer@sap.com>
2025-07-10 09:10:42 +0200

    trigger deploy preview

--- website/landingpage/dev/src/services/blogprocessor.js
+++ website/landingpage/dev/src/services/blogprocessor.js
@@ -7,7 +7,6 @@ import { BlogFeeds } from './feeds.service.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const luigiRootFolder = __dirname + '/../../../../../';
