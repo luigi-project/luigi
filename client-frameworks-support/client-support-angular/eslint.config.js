85bf185f74e18ad91d0ddb0558b96d71d6107e6f
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2025-07-21 09:08:44 +0200

    Migrates from TSlint to ESlint

---
+++ client-frameworks-support/client-support-angular/eslint.config.js
@@ -0,0 +1,41 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const stylistic = require('@stylistic/eslint-plugin');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = tseslint.config(
+  {
+    files: ['projects/**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      '@stylistic': stylistic
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': 'off',
+      '@angular-eslint/directive-selector': 'off',
+      '@angular-eslint/no-empty-lifecycle-method': 'off',
+      '@angular-eslint/prefer-inject': 'off',
+      '@angular-eslint/prefer-standalone': 'off',
+      '@typescript-eslint/consistent-indexed-object-style': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    },
+  },
+  {
+    files: ['projects/**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
