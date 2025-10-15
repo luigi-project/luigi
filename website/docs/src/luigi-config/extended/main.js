d8ea8959bed22f56999ab195381b40a484e7b734
hardl <philipp.pracht@sap.com>
2025-10-15 14:41:03 +0200

    theme store in cookie

--- website/docs/src/luigi-config/extended/main.js
+++ website/docs/src/luigi-config/extended/main.js
@@ -7,9 +7,45 @@ import { communication } from './communication';
 
 window.toggleTheme = () => {
   document.body.classList.toggle('lightTheme');
-  Luigi.theming().setCurrentTheme(document.body.classList.contains('lightTheme') ? 'light' : 'dark');
+  const currentTheme = document.body.classList.contains('lightTheme') ? 'light' : 'dark';
+  Luigi.theming().setCurrentTheme(currentTheme);
   Luigi.configChanged('navigation');
+  storeTheme(currentTheme);
 };
+
+function storeTheme(theme) {
+  const domainData = location.hostname.split('.');
+  let domain = location.hostname;
+  if (domainData.length > 1) {
+    domain = `${domainData[domainData.length - 2]}.${domainData[domainData.length - 1]}`;
+  }
+  document.cookie = `dark-mode=${theme === 'dark'}; Domain=${domain}; path=/; max-age=31536000 ; SameSite=lax`;
+}
+
+function readTheme() {
+  const biscuit = document.cookie;
+  if (biscuit && biscuit.length > 0) {
+    const vals = biscuit.split(';');
+    if (vals.length > 0) {
+      for (let index = 0; index < vals.length; index++) {
+        if (vals[index].trim() === 'dark-mode=false') {
+          return 'light';
+        } else if (vals[index].trim() === 'dark-mode=true') {
+          return 'dark';
+        }
+      }
+    }
+    else if (biscuit === 'dark-mode=false') {
+      return 'light';
+    }
+  }
+  return 'dark';
+}
+
+if (readTheme() === 'light') {
+  document.body.classList.toggle('lightTheme', true);
+  settings.theming.defaultTheme = 'light';
+}
 
 Luigi.setConfig({
   // auth,
