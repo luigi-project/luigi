04a3ac6a939ec9190fe03c8d3da3c952307deab1
JohannesDoberer <johannes.doberer@sap.com>
2025-10-20 15:13:41 +0200

    remove leftover

--- core-modular/src/types/connector.ts
+++ core-modular/src/types/connector.ts
@@ -16,7 +16,7 @@ export interface LuigiConnector {
 
   getContainerWrapper(): HTMLElement;
 
-  renderModal(content: HTMLElement, modalSettings: ModalSettings, onCloseCallback?: Function, modalHandler?: any): any;
+  renderModal(content: HTMLElement, modalSettings: ModalSettings, onCloseCallback?: Function): any;
 
   renderDrawer(content: HTMLElement, modalSettings: ModalSettings, onCloseCallback?: Function): any;
 
