53d5dd8438e394f720eb17059b817253f7e78702
hardl <philipp.pracht@sap.com>
2025-11-21 13:20:23 +0100

    test for store

---
+++ core-modular/test/utilities/store.spec.ts
@@ -0,0 +1,60 @@
+import { get, writable } from "../../src/utilities/store";
+
+describe('Luigi store', () => {
+    it('Check helper functions', () => {
+        const value = { some: 'object'};
+        const store = writable(value);
+        expect(get(store)).toEqual(value);
+    });
+
+    it('set', () => {
+        const value = { some: 'object'};
+        const store = writable('initialValue');
+        store.set(value);
+        expect(get(store)).toEqual(value);
+    });
+
+    it('update', () => {
+        const value = { some: 'object'};
+        const store = writable(value);
+        store.update((val) => {
+            val.some = 'updated value';
+            return val;
+        });
+        expect(get(store)).toEqual({ some: 'updated value'});
+    });
+
+    it('subscribe and unsubscribe', () => {
+        const value = { some: 'object'};
+        const store = writable(value);
+
+        const subscriber1 = jest.fn();
+        const subscriber2 = jest.fn();
+
+        const unsub1 = store.subscribe(subscriber1);
+        const unsub2 = store.subscribe(subscriber2);
+
+        expect(subscriber1).toHaveBeenCalledWith(value);
+        expect(subscriber2).toHaveBeenCalledWith(value);
+
+        unsub2();
+
+        subscriber1.mockReset();
+        subscriber2.mockReset();
+
+        store.set('updated');
+
+        expect(subscriber1).toHaveBeenCalledWith('updated');
+        expect(subscriber2).not.toHaveBeenCalled();
+
+        unsub1();
+
+        subscriber1.mockReset();
+        subscriber2.mockReset();
+
+        store.set('updated2');
+
+        expect(subscriber1).not.toHaveBeenCalled();
+        expect(subscriber2).not.toHaveBeenCalled();
+    });
+});
