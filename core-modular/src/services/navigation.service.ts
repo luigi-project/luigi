7bbbc0bda0f0c56105ac4dce4a177866a1e7a688
Waldemar Mazurek <waldemar.mazurek@sap.com>
2026-01-29 11:29:26 +0100

    openViewInNewTab in core modular (#4689)

--- core-modular/src/services/navigation.service.ts
+++ core-modular/src/services/navigation.service.ts
@@ -714,6 +714,43 @@ export class NavigationService {
     return { nodeObject, pathData };
   }
 
+  async shouldPreventNavigation(node: Node): Promise<boolean> {
+    if (
+      node?.onNodeActivation &&
+      (GenericHelpers.isFunction(node.onNodeActivation) || GenericHelpers.isAsyncFunction(node.onNodeActivation)) &&
+      (await node.onNodeActivation(node)) === false
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async shouldPreventNavigationForPath(nodepath: string): Promise<boolean> {
+    const { nodeObject } = await this.extractDataFromPath(nodepath);
+
+    if (await this.shouldPreventNavigation(nodeObject)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async openViewInNewTab(nodepath: string): Promise<void> {
+    if (await this.shouldPreventNavigationForPath(nodepath)) {
+      return;
+    }
+
+    const hashRouting = this.luigi.getConfigValue('routing.useHashRouting');
+
+    if (hashRouting) {
+      nodepath = '#' + nodepath;
+    }
+
+    /*'noopener,noreferrer' required to disable XSS injections*/
+    window.open(nodepath, '_blank', 'noopener,noreferrer');
+  }
+
   private resolveTooltipText(node: any, translation: string): string {
     return NavigationHelpers.generateTooltipText(node, translation, this.luigi);
   }
@@ -742,6 +779,7 @@ export class NavigationService {
     path: string,
     preserveView?: string,
     modalSettings?: any,
+    newTab?: boolean,
     callbackFn?: any
   ): Promise<void> {
     const normalizedPath = path.replace(/\/\/+/g, '/');
@@ -752,6 +790,12 @@ export class NavigationService {
       this.luigi.navigation().openAsModal(path, modalSettings, callbackFn);
     } else {
       await serviceRegistry.get(ModalService).closeModals();
+
+      if (newTab) {
+        await this.openViewInNewTab(path);
+        return;
+      }
+
       if (this.luigi.getConfig().routing?.useHashRouting) {
         location.hash = normalizedPath;
       } else {
