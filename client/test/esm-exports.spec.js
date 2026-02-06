3184d8a6f5e923c5a3cfe4ba016d53653220cea4
hardl <philipp.pracht@sap.com>
2026-02-06 16:45:32 +0100

    prettier

--- client/test/esm-exports.spec.js
+++ client/test/esm-exports.spec.js
@@ -5,15 +5,17 @@
 import * as ClientModule from '../src/esm-exports.js';
 
 describe('ES Module export tests', () => {
-    it('should export all expected functions', async () => {
-        expect(ClientModule).toBeDefined();
+  it('should export all expected functions', async () => {
+    expect(ClientModule).toBeDefined();
 
-        const LuigiClient = ClientModule.default;
-        expect(LuigiClient).toBeDefined();
+    const LuigiClient = ClientModule.default;
+    expect(LuigiClient).toBeDefined();
 
-        const exportedFunctions = Object.keys(ClientModule).filter(fn => fn !== 'default');
-        const luigiClientFunctions = Object.getOwnPropertyNames(Object.getPrototypeOf(LuigiClient)).filter(fn => fn !== 'constructor');
+    const exportedFunctions = Object.keys(ClientModule).filter((fn) => fn !== 'default');
+    const luigiClientFunctions = Object.getOwnPropertyNames(Object.getPrototypeOf(LuigiClient)).filter(
+      (fn) => fn !== 'constructor'
+    );
 
-        expect(new Set(exportedFunctions)).toEqual(new Set(luigiClientFunctions));
-    });
+    expect(new Set(exportedFunctions)).toEqual(new Set(luigiClientFunctions));
+  });
 });
