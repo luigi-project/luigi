51b4f5f0672d3dd0002e3d91d18f805cedfee4d4
JohannesDoberer <johannes.doberer@sap.com>
2026-02-25 12:03:43 +0100

    lcc

--- core-modular/src/modules/ui-module.ts
+++ core-modular/src/modules/ui-module.ts
@@ -39,6 +39,7 @@ const createContainer = async (node: any, luigi: Luigi): Promise<HTMLElement> =>
     lcc.theme = luigi.theming().getCurrentTheme();
     (lcc as any).viewGroup = node.viewGroup;
     (lcc as any).virtualTree = node.virtualTree || node._virtualTree;
+    (lcc as any).virtualTreeRootNode = NavigationHelpers.findVirtualTreeRootNode(node);
     luigi.getEngine()._comm.addListeners(lcc, luigi);
     return lcc;
   } else {
