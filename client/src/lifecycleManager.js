0a7e13bda9491c061c27bcb7f431a4ec941dc008
JohannesDoberer <johannes.doberer@sap.com>
2026-03-19 17:15:26 +0100

    release-v2.29.0

--- client/src/lifecycleManager.js
+++ client/src/lifecycleManager.js
@@ -494,7 +494,7 @@ class LifecycleManager extends LuigiClientBase {
 
    * @param {Object} searchParams
    * @param {boolean} keepBrowserHistory
-   * @param {boolean} preventLuigiConfigUpdate - If true, the configChanged function will be triggered (since NEXTRELEASE). By default it is set to `false`.
+   * @param {boolean} preventLuigiConfigUpdate - If true, the configChanged function will be triggered (since 2.29.0). By default it is set to `false`.
    * @memberof Lifecycle
    * @example
    * LuigiClient.addCoreSearchParams({luigi:'rocks'}, false);
