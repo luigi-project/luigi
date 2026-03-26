1fe99feb30c562f9e852aa907eb14cc5fcce482d
hardl <philipp.pracht@sap.com>
2026-03-26 08:14:26 +0100

    origin check

--- core/src/utilities/third-party-cookies-check.js
+++ core/src/utilities/third-party-cookies-check.js
@@ -5,6 +5,9 @@ let status = 'not_checked';
 EventListenerHelpers.addEventListener(
   'message',
   function(e) {
+    if (e.origin !== window.location.origin) {
+      return;
+    }
     if (e.data === 'luigi.tpcDisabled') {
       console.warn('Third party cookies are not supported! Silent token renewal might not work!');
       status = 'disabled';
--- plugins/auth/src/third-party-cookies-check.js
+++ plugins/auth/src/third-party-cookies-check.js
@@ -4,6 +4,9 @@ let status = 'not_checked';
 window.addEventListener(
   'message',
   function(e) {
+    if (e.origin !== window.location.origin) {
+      return;
+    }
     if (e.data === 'luigi.tpcDisabled') {
       console.warn(
         'Third party cookies are not supported! Silent token renewal might not work!'
