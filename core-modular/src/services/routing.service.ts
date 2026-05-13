5b803234f2649050d59efa57c23a67788059dafa
JohannesDoberer <johannes.doberer@sap.com>
2026-05-13 12:51:43 +0200

    intendToHaveEmptyViewUrl check in handlePageNotFound
    Signed-off-by: JohannesDoberer <johannes.doberer@sap.com>

--- core-modular/src/services/routing.service.ts
+++ core-modular/src/services/routing.service.ts
@@ -471,7 +471,7 @@ export class RoutingService {
     const activePath: string = GenericHelpers.getTrimmedUrl(path);
     const isExistingRoute: boolean = RoutingHelpers.isExistingRoute(activePath, pathData);
 
-    if ((!viewUrl && !nodeObject?.compound) || nodeObject?.tabNav?.showAsTabHeader) {
+    if ((!viewUrl && !nodeObject?.compound && !nodeObject?.intendToHaveEmptyViewUrl) || nodeObject?.tabNav?.showAsTabHeader) {
       const defaultChildNode = await RoutingHelpers.getDefaultChildNode(pathData, async (node, ctx) => {
         return await this.getNavigationService().getChildren(node, ctx);
       });
