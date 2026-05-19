9df7bd0f76eee1af1f77189acd62ef3cbbe778cb
hardl <philipp.pracht@sap.com>
2026-05-19 17:17:18 +0200

    prettier
    Signed-off-by: hardl <philipp.pracht@sap.com>

--- client-frameworks-support/client-support-angular/jest.config.js
+++ client-frameworks-support/client-support-angular/jest.config.js
@@ -6,15 +6,14 @@ module.exports = {
   roots: ['projects/client-support-angular/src'],
   testMatch: ['**/*.spec.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'projects/client-support-angular/tsconfig.spec.json'
-    }]
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'projects/client-support-angular/tsconfig.spec.json'
+      }
+    ]
   },
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: [
-    'projects/client-support-angular/src/**/*.ts',
-    '!**/*.spec.ts',
-    '!**/test.ts'
-  ]
+  collectCoverageFrom: ['projects/client-support-angular/src/**/*.ts', '!**/*.spec.ts', '!**/test.ts']
 };
