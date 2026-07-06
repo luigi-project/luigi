25981cf1d8badae6a00a51c6db23a8efe673636d
hardl <philipp.pracht@sap.com>
2026-07-06 09:31:23 +0200

    replace (a)live-server in tests
    Signed-off-by: hardl <philipp.pracht@sap.com>

---
+++ client-frameworks-support/testing-utilities/test/dev-server.mjs
@@ -0,0 +1,8 @@
+import { startSimpleServer } from '../../../scripts/tools/simple-server/index.mjs';
+
+startSimpleServer({
+  port: 8181,
+  root: '.',
+  mounts: [['/node_modules', './node_modules']],
+  watch: ['.']
+});
