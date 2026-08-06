690b6f8fceb5978e8804d6528d53437feb5c85c7
Waldemar Mazurek <waldemar.mazurek@sap.com>
2026-08-06 11:46:21 +0200

    withoutSync issue in core modular (#5387)

--- core-modular/src/modules/communicaton-module.ts
+++ core-modular/src/modules/communicaton-module.ts
@@ -165,7 +165,9 @@ export const CommunicationModule = {
       serviceRegistry
         .get(NavigationService)
         .getCurrentRoutePath(options)
-        .then((route) => {event.callback(route)})
+        .then((route) => {
+          event.callback(route);
+        })
         .catch(() => event.callback(''));
     });
     containerElement.addEventListener(Events.SET_VIEW_GROUP_DATA_REQUEST, (event: LuigiEvent) => {
