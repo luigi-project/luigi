46f6faefcc7d4344d8bcf8395f8f2cd542500fb1
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2026-09-23 15:04:08 +0200

    Fixes top nav config
    Signed-off-by: Waldemar Mazurek <waldemar.mazurek@accenture.com>

--- core-modular/src/services/navigation.service.ts
+++ core-modular/src/services/navigation.service.ts
@@ -683,11 +683,18 @@ export class NavigationService {
       };
     }
 
+    const logoConfig = cfg.settings?.header?.logo;
+
+    if (logoConfig && typeof cfg.settings?.header?.altText === 'string') {
+      logoConfig.alt = this.luigi.i18n().getTranslation(cfg.settings.header.altText);
+    }
+
     return {
       appTitle: headerTitle || cfg.settings?.header?.title,
+      favicon: cfg.settings?.header?.favicon,
       globalSearch,
       isHeaderDisabled: !!cfg.settings?.header?.disabled,
-      logo: cfg.settings?.header?.logo,
+      logo: logoConfig,
       topNodes: navData.items,
       totalBadgeNode: navData.totalBadgeNode,
       contextSwitcher,
